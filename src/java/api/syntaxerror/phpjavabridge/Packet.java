49e25d40ef8a94f9e876188d55ed75bd58318217
Synt4xErr0r4 <45700073+Synt4xErr0r4@users.noreply.github.com>
2020-07-31 23:45:28 +0200

    added file header

--- src/java/api/syntaxerror/phpjavabridge/Packet.java
+++ src/java/api/syntaxerror/phpjavabridge/Packet.java
@@ -1,5 +1,40 @@
 package api.syntaxerror.phpjavabridge;
 
+/**
+ * PHP-Java-Bridge provides a TCP- or UDP-based connection between PHP (Client) and Java (Server)<br>
+ * <br>
+ * licensed under the Apache License 2.0:<br>
+ * <br>
+ * Permissions:
+ * <ul>
+ *  <li>Commercial use</li>
+ *  <li>Modification</li>
+ *  <li>Distribution</li>
+ *  <li>Patent use</li>
+ *  <li>Private use</li>
+ * </ul>
+ * 
+ * Limitiations:
+ * <ul>
+ *  <li>Trademark use</li>
+ *  <li>Liability</li>
+ *  <li>Warranty</li>
+ * </ul>
+ * 
+ * Conditions:
+ * <ul>
+ *  <li>License and copyright notice</li>
+ *  <li>State changes</li>
+ * </ul>
+ * 
+ * License: <a href=https://github.com/Synt4xErr0r4/PHP-Java-Bridge/blob/master/LICENSE>https://github.com/Synt4xErr0r4/PHP-Java-Bridge/blob/master/LICENSE</a><br>
+ * GitHub Repository: <a href=https://github.com/Synt4xErr0r4/PHP-Java-Bridge/>https://github.com/Synt4xErr0r4/PHP-Java-Bridge/</a><br>
+ * Wiki: <a href=https://github.com/Synt4xErr0r4/PHP-Java-Bridge/wiki>https://github.com/Synt4xErr0r4/PHP-Java-Bridge/wiki</a><br>
+ * JavaDocs (for /src/java): <a href=https://github.com/Synt4xErr0r4/PHP-Java-Bridge/blob/master/javadoc/>https://github.com/Synt4xErr0r4/PHP-Java-Bridge/blob/master/javadoc/</a><br>
+ * 
+ * @version 1.0
+ * @author SyntaxError404, 2020
+ */
 public class Packet {
 	
 }
